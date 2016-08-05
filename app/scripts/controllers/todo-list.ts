c6bde1cd0ec4ce2d065efcd9b963cdb14780d47e
Roberto De la Parra <rdelaparral@psl.com.co>
2016-08-05 16:28:23 -0500

    advance TodoListController.

--- app/scripts/controllers/todo-list.ts
+++ app/scripts/controllers/todo-list.ts
@@ -1,7 +1,52 @@
+'use strict'
+namespace app{
+  interface ITodoListController{
+    scope: ng.IScope;
+    todoService: any;
+  }
+
+  class TodoListController implements ITodoListController{
+    scope: ng.IScope;
+    todoService: any;
+
+    static $inject = ["$scope", "todoService"];    
+    constructor($scope: ng.IScope, todoService: any) {
+      this.scope = $scope;
+      this.todoService = todoService;
+      this.scope.todoList = [];
+    }
+
+    public markAsUncompleted(todo) : void {
+      todo.completed = false;      
+      this.todoService.update({id : todo.id}, todo, function(){}, function(){
+        //on error
+        todo.completed = true;
+      });
+    }
+ 
+    public markAsCompleted(todo) : void {
+      todo.completed = true;
+      this.todoService.update({id : todo.id}, todo, function(){}, function(){
+        //on error
+        todo.completed = false;
+      });
+    }
+
+    public deleteTodo(todo, index): void{
+      this.todoService.delete({id: todo.id}, todo, function(){
+          this.scope.todoList.splice(index,1);
+      });
+    }
+  }
+
+  angular.module('todoApp')
+         .controller('TodoListController', TodoListController);
+}
+/*
 angular.module('todoApp').controller('TodoListController', function($scope, todoService) {
   $scope.todoList = [];
   todoService.query(function(data){
-     $scope.todoList = data;
+     $scope.todoList = data; //???
   });
   
   $scope.markAsUncompleted = function(todo){
@@ -29,4 +74,4 @@ angular.module('todoApp').controller('TodoListController', function($scope, todo
       $scope.todoList.splice(index,1);
     });
   }  
-});
+});*/
