4a44f7081db43102243381fb2ad1537bc05ee61e
Roberto De la Parra <rdelaparral@psl.com.co>
2016-08-19 10:46:20 -0500

    removed inject in maincontroller

--- app/scripts/controllers/main.ts
+++ app/scripts/controllers/main.ts
@@ -8,14 +8,14 @@
  * Controller of the todoApp
  */
 
-namespace app{
+namespace app {
   class MainController{
     private awesomeThings: Array<String> = [
       'HTML Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    static $inject = [];
+    
   }
 
   angular.module('todoApp').controller('MainController', MainController);
